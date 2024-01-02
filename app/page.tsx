5f901dfba1589fd38f9e2b5aaa8766ffd3b2a56e
Ryan Schock <ryanjms613@gmail.com>
2024-01-02 18:30:12 -0500

    Extended carousel to recommend similar tv shows + fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,8 @@ export default async function Home() {
                 poster_path={movie.poster_path}
                 release_date={movie.release_date}
                 vote_average={movie.vote_average}
-                index={index}
+                original_language={movie.original_language}
+                backdrop_path={movie.backdrop_path}
                 status={movie.status}
                 name={movie.name}
               />
